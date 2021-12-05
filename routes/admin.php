2a5ed68125e25d2c70f3ccaee85c33667b296f8b
Eslam-atia442 <56205474+Eslam-atia442@users.noreply.github.com>
2021-12-05 15:38:34 +0200

    tt

--- routes/admin.php
+++ routes/admin.php
@@ -7,7 +7,7 @@ Route::group(['namespace' => 'Admin', 'as' => 'admin.'], function () {
     /*authentication*/
     Route::group(['namespace' => 'Auth', 'prefix' => 'auth', 'as' => 'auth.'], function () {
         Route::get('login', 'LoginController@login')->name('login');
-        Route::post('login', 'LoginController@submit');
+        Route::post('login', 'LoginController@submit')->middleware('actch');
         Route::get('logout', 'LoginController@logout')->name('logout');
     });
     /*authentication*/
